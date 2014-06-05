69096c9d0d3651e3841471f1695c7a837a9b5d10
clwillingham <clwillingham@gmail.com>
2014-06-04 20:28:47 -0400

    adding team model and user and admin stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :teams
+
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  # get 'home/index'
+  root 'home#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
