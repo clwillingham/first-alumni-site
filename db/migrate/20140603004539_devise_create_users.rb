9abe1d57714347a76f1a45e4d68acae5139439b3
clwillingham <clwillingham@gmail.com>
2014-06-04 21:24:49 -0400

    adding is_admin column to user model, and adding league column to team model

--- db/migrate/20140603004539_devise_create_users.rb
+++ db/migrate/20140603004539_devise_create_users.rb
@@ -30,6 +30,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      #custom data
+      t.boolean :is_admin, default: false
 
       t.timestamps
     end
--- db/migrate/20140603010153_create_teams.rb
+++ db/migrate/20140603010153_create_teams.rb
@@ -2,7 +2,8 @@ class CreateTeams < ActiveRecord::Migration
   def change
     create_table :teams do |t|
       t.integer :number
-      t.string :name
+      t.string  :name
+      t.string  :league
       t.timestamps
     end
   end
